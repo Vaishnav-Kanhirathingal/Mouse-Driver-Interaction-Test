7b729f6d3058bc560a546c6a0311148b536caeb6
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-02-13 11:57:15 +0530

    code optimization

--- Test_1.cpp
+++ Test_1.cpp
@@ -1,22 +1,19 @@
-#include<winuser.h>
-#include<conio.h>
-#include<iostream>
-#include<stdio.h>
+#include<windows.h>
 
 void moveMouse(int x, int y) {
     INPUT input;
     input.type = INPUT_MOUSE;
-    input.mi.mouseData = 0;
     input.mi.dx = x;
     input.mi.dy = y;
+    input.mi.time = 0;
     input.mi.dwFlags = MOUSEEVENTF_MOVE;
     SendInput(1, &input, sizeof(input));
-}
+}              
 
 int main(int argc, char* argv[]) {
-    for (int i = 0;i < 500;i++) {
+    for (int i = 0;i < 100;i++) {
         moveMouse(10, 0);
-        _sleep(20);
+        Sleep(20);
     }
     return 1;
 }
